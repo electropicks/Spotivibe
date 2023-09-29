1fea9769b62c90986909d6535e9cf174bcc180f3
Liam Kbidy <lkbidy@amazon.com>
2023-09-29 10:21:50 -0700

    initial commit (typescript + react)

---
+++ src/components/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const TestButton = () => {
+    return <Button variant="contained">Hello World!</Button>;
+}
+
+export default TestButton;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./components/Home";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+);
+
