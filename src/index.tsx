232db6433060c133265b022bc98435c552b41cab
electropicks <lkbidy@gmail.com>
2023-10-02 08:23:44 -0700

    Initial commits

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./components/Home";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
 );
 
---
+++ vite.env.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="vite/client" />
+/// <referencetypes="vite-plugin-svgr/client" />
