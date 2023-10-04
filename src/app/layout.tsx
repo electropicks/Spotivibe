49fd991eec857aea955a24e291f4e08ab4b888c7
electropicks <lkbidy@gmail.com>
2023-10-04 08:42:15 -0700

    dev commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({subsets: ['latin']})
 
@@ -18,7 +17,6 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={inter.className}>{children}
-        <Analytics/>
         </body>
         </html>
     )
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <h1>Test from index page</h1>
+    )
+}
